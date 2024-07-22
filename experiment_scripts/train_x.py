3447a526cd08afcf062f072d19147c7aa60cbd20
Bowen-zhang96 <142164965+Bowen-zhang96@users.noreply.github.com>
2024-07-22 10:41:50 +0100

    Add files via upload

--- experiment_scripts/train_x.py
+++ experiment_scripts/train_x.py
@@ -37,7 +37,7 @@ def main(args):
             args.exp_name = response.lower()
 
     lpips_fn = losses.LPIPS_1D()
-    args, dir_name = utils.modify_args(args)
+    args, dir_name,restore_name = utils.modify_args(args)
 
     # define model
     model_dir, version_num = utils.make_model_dir(dir_name, args.test, args.exp_name)
@@ -65,20 +65,48 @@ def main(args):
     optim1 = torch.optim.SGD([
         {'params': model.shutter.parameters(), 'lr': args.slr}], lr=args.mlr, momentum=0.9)
 
-
-
     if args.date_resume != '00-00-00':
         print('Loading checkpoint')
-        file = [f for f in os.listdir(f'{dir_name}/v_{34}/checkpoints') if 'model_epoch_0006.pth' in f][0]
-        fname = f'{dir_name}/v_{34}/checkpoints/{file}'
+        file = [f for f in os.listdir(f'{restore_name}/v_{4}/checkpoints') if 'model_epoch_0133.pth' in f][0]
+        fname = f'{restore_name}/v_{4}/checkpoints/{file}'
+        # file = [f for f in os.listdir(f'{restore_name}/v_{42}/checkpoints') if 'model_epoch_0120.pth' in f][0]
+        # fname = f'{restore_name}/v_{42}/checkpoints/{file}'
         print(fname)
         checkpoint = torch.load(fname)
+        # model.load_state_dict(checkpoint['model_state_dict'], strict=False)
+        # optim.load_state_dict(checkpoint['optim_state_dict'])
+        #  try:
+        #    current_model_dict = model.state_dict()
+        # enabling this if its for training
+        # print(checkpoint['model_state_dict'].keys())
 
-    
-        model.load_state_dict(checkpoint['model_state_dict'], strict=False)
-        optim.load_state_dict(checkpoint['optim_state_dict'])
-     
 
+        if 'adaptive' in restore_name:
+            new_state_dict={k:v for k,v in zip(checkpoint['model_state_dict'].keys(), checkpoint['model_state_dict'].values()) if 'shutter.model.body.4' not in k}
+            model.load_state_dict(new_state_dict, strict=False)
+        else:
+            new_state_dict = {k: v for k, v in
+                              zip(checkpoint['model_state_dict'].keys(), checkpoint['model_state_dict'].values()) if
+                              'shutter.model.body' not in k}
+            model.load_state_dict(new_state_dict, strict=False)
+
+
+
+        #        new_state_dict={k:v for k,v in zip(checkpoint['optim_state_dict'].keys(), checkpoint['optim_state_dict'].values()) if 'shutter.model.body.2' not in k}
+        #       optim.load_state_dict(new_state_dict)
+        # some baselines won't have optim state dict, doesn't affect training much  shutter.model.body.2.body.4
+        # new_state_dict = {k: v for k, v in
+        #                   zip(checkpoint['model_state_dict'].keys(), checkpoint['model_state_dict'].values()) if
+        #                   'shutter.model.body' not in k}
+        # model.load_state_dict(new_state_dict, strict=False)
+
+        # model.load_state_dict(checkpoint['model_state_dict'], strict=False)
+    #     optim.load_state_dict(checkpoint['optim_state_dict'])
+    #     optim1.load_state_dict(checkpoint['optim1_state_dict'])
+    #    except KeyError:
+    #   current_model_dict = model.state_dict()
+    # new_state_dict={k:v for k,v in zip(current_model_dict.keys(), current_model_dict.values()) if 'shutter.model.body.4.weight' not in k}
+    #        model.load_state_dict(checkpoint, strict=False)
     else:
         if not args.test:
             with open(f'{dir_name}/args.json', 'w') as f:
@@ -86,93 +114,157 @@ def main(args):
 
     train_dataloader = dataloading.loadTrainingDataset(args)
     val_dataloader, val_dataset = dataloading.loadValDataset(args)
-    vid_dict=val_dataset.vid_dict
     summary_fn = partial(summary_utils.write_summary, args.batch_size, writer, args.shutter)
 
+    scheduler = utils.define_schedule(optim)
+    scheduler1 = torch.optim.lr_scheduler.MultiStepLR(optim1, milestones=[50, 100, 150, 200, 250], gamma=0.5)
 
     loss_fn = utils.define_loss(args)
 
+    total_time_start = time.time()
+    best_val_psnr = 0
+    total_steps = 0
 
-    
-    model.eval()
-    val_losses = []
-    val_psnrs = []
-    val_lpips = []
-    val_ssims = []
-    val_actions=[]
-    
-    restored_s=[]
-    gt_s=[]
-    actions_s=[]
-    idx=0
-    files_dict=[]
-    for (avg, model_input, gt, ref_input, vid_num, clip_num) in tqdm(val_dataloader, disable=False):
-        vid_num=vid_num.detach().cpu().numpy()
-        clip_num=clip_num.detach().cpu().numpy()
-        for tmp in range(vid_num.shape[0]):
-            
-            vid = vid_dict[vid_num[tmp]][clip_num[tmp]]
-            
-            
-            vid=vid[8]
-           
-            files_dict.append(vid)
-        idx=idx+1
-    #     if idx>50:  args.batch_size
-   #          break
-        model_input = model_input.cuda()
-        gt = gt.cuda()
-        ref_input=ref_input.cuda()
-        restored, coded, actions, rate_loss = model([model_input,ref_input], train=False)
-        
-        
+    f = open('progress.txt', "w")
+    f.write("I have deleted the content!")
+    f.close()
 
-        val_loss = loss_fn(restored, gt)
-        psnr = summary_utils.get_psnr(restored, gt)
-        ssim = summary_utils.get_ssim(restored, gt)
+    with tqdm(total=len(train_dataloader) * args.max_epochs) as pbar:
+        for epoch in range(args.max_epochs):
+            print(epoch)
+            for step, (avg, model_input, gt, ref_input, vid_num, clip_num) in enumerate(train_dataloader):
+
+                model_input = model_input.cuda()
+                gt = gt.cuda()
+                ref_input = ref_input.cuda()
+                start_time = time.time()
+
+                optim.zero_grad(set_to_none=True)
+                optim1.zero_grad(set_to_none=True)
+
+                train_losses = []
+                # for itr in range(1):
+                if epoch<120:
+                    restored, coded, actions, log_prob = model([model_input, ref_input], train=True, steps=total_steps)
+                else:
+                    restored, coded, actions, log_prob = model([model_input, ref_input], train=False, steps=total_steps)
+
+                actions = actions.float()
+                y = torch.zeros_like(actions)
+
+                    #  y = torch.where(actions == -1, 0*torch.ones_like(actions), y)
+                y = torch.where(actions == 0, 1 * torch.ones_like(actions), y)
+                y = torch.where(actions == 1, 2 * torch.ones_like(actions), y)
+                y = torch.where(actions == 2, 4 * torch.ones_like(actions), y)
+                y = torch.where(actions == 3, 8 * torch.ones_like(actions), y)
+
+                    #      train_loss1=0
+
+                train_loss1 = (restored - gt).pow(2).mean(dim=[1, 2, 3])
+
+                    #    if log_prob is None:
+                    #        train_loss = train_loss1
+                    #    else:
+                    #        rewards=(torch.log(1./((restored - gt).pow(2).mean(dim=[1,2,3]).detach()+1e-6)))
+
+                    #        train_loss = train_loss1-(rewards-10.*y.mean(dim=[1,2,3]).detach())*(log_prob.mean(dim=[1,2,3]))
+                if log_prob is None:
+                    train_loss = train_loss1
+                    train_losses.append(train_loss)
+                else:
+                    rewards = (torch.log(1. / ((restored - gt).pow(2).detach() + 1e-4)))
+
+                    train_loss = train_loss1 - ((rewards - 0.05 * y.detach()) * log_prob).mean()
+
+                    train_losses.append(train_loss)
+                # train_loss2= (coded[:,:-1,:,:]-gt).pow(2).mean()-4.*actions.detach()  -0.1*train_loss2.mean() +-0.01*y.detach()  0.008   0.012 0.03
+                # gamma 1. Action 1.817
+                # print(train_loss)
+                train_loss = torch.concat(train_losses, dim=0).mean()
+
+                train_loss.backward()
+                # v=model.shutter.model[-1][-2].weight.grad.detach().cpu().numpy()
+                # print(np.mean(np.abs(v)))
+                # v = model.shutter.end_params.grad.detach().cpu().numpy()
+                # print(np.mean(np.abs(v)))
+                optim.step()
+                optim1.step()
+                pbar.update(1)
+                if not total_steps % args.steps_til_summary and total_steps > 50:
+                    writer.add_scalar("total_train_loss", train_loss, total_steps)
+                    # summary_fn(model, [model_input[:1], ref_input[:1]], gt[:1, :, :, :], restored[:1, :, :, :], avg,
+                    #            total_steps, optim)
+                    tqdm.write("Epoch %d, Total loss %0.6f, "
+                               "iteration time %0.6f s, total time %0.6f min" % (
+                                   epoch, train_loss, time.time() - start_time,
+                                   (time.time() - total_time_start) / 60.0))
+
+                    if val_dataloader is not None:
+                        with torch.no_grad():
+                            model.eval()
+                            val_losses = []
+                            val_psnrs = []
+                            val_lpips = []
+                            val_ssims = []
+                            val_actions = []
+                            for (avg, model_input, gt, ref_input, vid_num, clip_num) in tqdm(val_dataloader, disable=True):
+                                model_input = model_input.cuda()
+                                gt = gt.cuda()
+                                ref_input = ref_input.cuda()
+                                restored, coded, actions, rate_loss = model([model_input, ref_input], train=False)
+                                #  restored=restored.reshape([-1,5,16,args.block_size[1],args.block_size[2]])
+                                #   restored=restored[-1]
+
+                                val_loss = loss_fn(restored, gt)
+                                psnr = summary_utils.get_psnr(restored, gt)
+                                ssim = summary_utils.get_ssim(restored, gt)
                                 # vlpips = lpips_fn(restored, gt)
-        actions=actions.float()
-        y = torch.zeros_like(actions)
+                                # actions=torch.round(actions)
+                                actions = actions.float()
+                                y = torch.zeros_like(actions)
 
-                              #  y = torch.where(actions == -1, 0*torch.ones_like(actions), y)
-        y = torch.where(actions == 0, 1*torch.ones_like(actions), y)
-        y = torch.where(actions == 1, 2*torch.ones_like(actions), y)
-        y = torch.where(actions == 2, 4*torch.ones_like(actions), y)
-        y = torch.where(actions == 3, 8*torch.ones_like(actions), y)
-        y = torch.where(actions == 4, 16*torch.ones_like(actions), y)
-                                
+                                #  y = torch.where(actions == -1, 0*torch.ones_like(actions), y)
+                                y = torch.where(actions == 0, 1 * torch.ones_like(actions), y)
+                                y = torch.where(actions == 1, 2 * torch.ones_like(actions), y)
+                                y = torch.where(actions == 2, 4 * torch.ones_like(actions), y)
+                                y = torch.where(actions == 3, 8 * torch.ones_like(actions), y)
 
-        val_ssims.append(ssim)
-        val_psnrs.append(psnr)
-        val_losses.append(val_loss.item())
-        val_lpips.append(0)
-        val_actions.append(y.mean().detach().cpu().numpy())
-        
-        restored_s.append(restored.detach().cpu().numpy())
-        gt_s.append(gt.detach().cpu().numpy())
-        # print(actions[0,0,:,:])
-        actions_s.append(y.detach().cpu().numpy())
+                                val_ssims.append(ssim)
+                                val_psnrs.append(psnr)
+                                val_losses.append(val_loss.item())
+                                val_lpips.append(0)
+                                val_actions.append(y.mean().detach().cpu().numpy())
+                                # print(f'PSNR: '
+                                #       f'{np.mean(val_psnrs)}, SSIM: {np.mean(val_ssims)}, LPIPS: {np.mean(val_lpips)}, Action:{np.mean(val_actions)}')
 
-        # val_ssims.append(ssim)
-        # val_psnrs.append(psnr)
-        # val_losses.append(val_loss.item())
-        # val_lpips.append(0)
+                            # summary_utils.write_val_scalars(writer,
+                            #                                 ['psnr', 'ssim', 'lpips', 'loss'],
+                            #                                 [val_psnrs, val_ssims, val_lpips, val_losses],
+                            #                                 total_steps)
+                            utils.save_chkpt(model, optim, optim1, checkpoints_dir, epoch=epoch, final=False)
 
-       # summary_utils.write_val_scalars(writer, ['psnr', 'ssim', 'lpips', 'loss'],[val_psnrs, val_ssims, val_lpips, val_losses],
-       # total_steps)
+                            # if np.mean(val_psnrs) > best_val_psnr:
+                            print(f'PSNR: '
+                                  f'{np.mean(val_psnrs)}, SSIM: {np.mean(val_ssims)}, LPIPS: {np.mean(val_lpips)}, Action:{np.mean(val_actions)}')
+                            #  best_val_psnr = np.mean(val_psnrs)
+                            f = open('progress.txt' , "a")
+                            f.write(f'#Test iteration %d, ,'
+                                    f'PSNR %.8f, SSIM %.8f, LPIPS %.8f, Action %.8f #\n' % (
+                                    epoch,
+                                    np.mean(val_psnrs),
+                                    np.mean(val_ssims),
+                                    np.mean(val_lpips),
+                                    np.mean(val_actions)
+                                    ))
+                            f.close()
 
-        
-    print(f'BEST PSNR: 'f'{np.mean(val_psnrs)}, SSIM: {np.mean(val_ssims)}, Action:{np.mean(val_actions)}')
-    
-    restored_s=np.concatenate(restored_s,axis=0)
-    gt_s=np.concatenate(gt_s,axis=0)
-    actions_s=np.concatenate(actions_s,axis=0)
-    idx=np.arange(len(restored_s))
-    print(idx)
-    np.random.shuffle(idx)
-    idx=idx[:20]
-    sio.savemat('results_adaptive_PSNR_%.5f_Action_%.5f.mat'%(np.mean(val_psnrs), np.mean(val_actions)),{'restored':restored_s[idx], 'gt':gt_s[idx],'action':actions_s[idx],'files':np.take(files_dict,idx)})
-                                
+                            utils.save_chkpt(model, optim, optim1, checkpoints_dir, epoch=epoch, best=True)
+                            utils.save_best_metrics(args, total_steps, epoch, val_psnrs,
+                                                    val_ssims, val_lpips, checkpoints_dir)
+                        model.train()
+                        scheduler.step(val_loss)
+                        scheduler1.step()
+                total_steps += 1
 
 
 if __name__ == '__main__':
@@ -196,16 +288,17 @@ if __name__ == '__main__':
     parser.add_argument('--local', action='store_true')
     parser.add_argument('--scale', type=int, default=0)
     parser.add_argument('--exp_name', type=str, default='MST_adaptive')
+    parser.add_argument('--restore_dic_name', type=str, default='MST_fixed')
     parser.add_argument('--reg', type=float, default=100.0, help='regularization on lpips loss')
     parser.add_argument('--k', type=int, default=3)
     parser.add_argument('--p', type=float, default=1.0)
-    parser.add_argument('--max_epochs', type=int, default=6000)
+    parser.add_argument('--max_epochs', type=int, default=300)
 
     parser.add_argument('--mlr', help='model_lr', type=str, default='5e-5')
     parser.add_argument('--slr', help='shutter_lr', type=str, default='5e-3')
     parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--num_workers', type=int, default=4)
-    parser.add_argument('--steps_til_summary', type=int, default=1000)
+    parser.add_argument('--steps_til_summary', type=int, default=500)
     parser.add_argument('--epochs_til_checkpoint', type=int, default=1000)
     parser.add_argument('--steps_til_ckpt', type=int, default=5000)
     parser.add_argument('--interp', type=str, default='none',
